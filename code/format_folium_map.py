170c8859020eb0befec3f4618b1aa769e980ae5a
cerickson30 <62108211+cerickson30@users.noreply.github.com>
2020-11-29 14:39:47 -0600

    Add files via upload

--- code/format_folium_map.py
+++ code/format_folium_map.py
@@ -9,7 +9,7 @@ def add_title_legend(mymap, year, dem_name, repub_name, other_name):
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>jQuery UI Draggable - Default functionality</title>
+      <title>The Purple States of America</title>
       <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
       <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
